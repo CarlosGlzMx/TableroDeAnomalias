255e74649b3a5c6ce4aa8a21743c75bf8477a573
Luis Leyva <a01570367@itesm.mx>
2022-05-19 17:25:58 -0500

    Conexion Inicial Upload con API

--- tablero-anomalias/src/components/Upload.jsx
+++ tablero-anomalias/src/components/Upload.jsx
@@ -26,9 +26,11 @@ function Upload(user) {
 	}, [type, isValid, data]);
 
 	return (
-		<div className="Upload" style={{display:"flex", alignItems:"center", justifyContent:"center", backgroundImage: `url(${AnomalyBg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-			<div className="p-4" style={{ width: "40%", height: "66vh", margin: "8vh", border:"0.3rem dashed #6C757D",
-					borderRadius: "0.5rem", backgroundColor: "#EEEEEE"}}>
+		<div className="Upload" style={ { display: "flex", alignItems: "center", justifyContent: "center", backgroundImage: `url(${AnomalyBg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" } }>
+			<div className="p-4" style={ {
+				width: "40%", height: "66vh", margin: "8vh", border: "0.3rem dashed #6C757D",
+				borderRadius: "0.5rem", backgroundColor: "#EEEEEE"
+			} }>
 				<Form validated={ isValid } >
 					<Form.Group controlId="formFile" className="mb-3">
 						<Form.Label>Carga un archivo de tipo .csv o .xlsx</Form.Label>
@@ -37,6 +39,7 @@ function Upload(user) {
 							isInvalid={
 								isValid === undefined ? null : !isValid
 							}
+							accept=".csv, .xlsx"
 							type="file"
 							onChange={ (e) => {
 								setType(e.target.files[0].type);
@@ -50,22 +53,22 @@ function Upload(user) {
 				</Form>
 				<Link
 					to="/selectColumn"
-					state={{
+					state={ {
 						data,
 						type
-					}}>
+					} }>
 					<Button
 						style={ {
 							backgroundColor: "#ff8300",
 							border: "none"
 						} }
 						size="sm"
-						disabled={isValid === undefined ? true : !isValid} >
+						disabled={ isValid === undefined ? true : !isValid } >
 						Seleccionar Columnas
 					</Button>
 				</Link>
 			</div>
-			<div style={{ width: "40%", height: "66vh", margin: "8vh", border:"0.3rem dashed #ff8300", borderRadius: "0.5rem", backgroundColor:"white"}}>
+			<div style={ { width: "40%", height: "66vh", margin: "8vh", border: "0.3rem dashed #ff8300", borderRadius: "0.5rem", backgroundColor: "white" } }>
 				<div className="w-100 h-50 p-4">
 					<div className="h4">Cargas disponibles</div>
 					<ul>
