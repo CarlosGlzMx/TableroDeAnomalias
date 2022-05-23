dd98cea9dc167ab0515332653cd1253f80010142
Carlos G. del Rosal <carlosglzmx@gmail.com>
2022-05-22 20:27:42 -0500

    Boton de acciones upload

--- tablero-anomalias/src/components/Actions.jsx
+++ tablero-anomalias/src/components/Actions.jsx
@@ -72,14 +72,14 @@ function Actions() {
                 </button>
 
 
-                <button class="btn btn-default" onClick={() => { printPdf() }}>
+                <button class="btn btn-default" onClick={() => printPdf() }>
                     <svg xmlns="http://www.w3.org/2000/svg" width="6vh" fill="currentColor" class="bi bi-file-earmark-bar-graph" viewBox="0 0 16 16">
                         <path d="M10 13.5a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-6a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v6zm-2.5.5a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1zm-3 0a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-1z" />
                         <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
                     </svg>
                 </button>
 
-                <button class="btn btn-default">
+                <button class="btn btn-default" onClick={ () =>  window.location.href = '/upload' }>
                     <svg xmlns="http://www.w3.org/2000/svg" width="6vh" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
                         <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                         <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" />
