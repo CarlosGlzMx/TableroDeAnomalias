6b5a90ce2434997ce9563276b8b38f4fc6359bb7
eduardo-2307 <a01562053@itesm.mx>
2022-05-24 22:46:08 -0500

    segunda grafica funcionando

--- tablero-anomalias/src/components/charts/Chart3.jsx
+++ tablero-anomalias/src/components/charts/Chart3.jsx
@@ -3,19 +3,24 @@ import { scaleOrdinal } from 'd3-scale';
 import { schemeCategory10 } from 'd3-scale-chromatic';
 import { BarChart, Bar, Cell,ResponsiveContainer } from "recharts";
 import { DataContext } from "../../App";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
   
 function Chart3() {
 
-  const [data6, setData] = useState([{
-    name: '-1',
-    number: 10
-  },
-  {
-    name: '-0.9',
-    number: 12
-  }])
+  const [data6, setData] = useState([
+    {
+      name: '-1',
+      number: 23
+    },
+    {
+      name: '-0.9',
+      number: 45
+    },
+    {
+      name: '0.0',
+      number: 34
+    }
+  ])
 
       // const data6 = [
       //   {
@@ -72,8 +77,10 @@ function Chart3() {
     // #4 Proceso de cada gráfica
     const UMBRAL_ANOMALIA = 0;
     let [A_1_0, A_0_9, A_0_8, A_0_7, A_0_6, A_0_5, A_0_4, A_0_3, A_0_2, A_0_1, A0, A0_1, A0_2, A0_3, A0_4, A0_5, A0_6, A0_7, A0_8, A0_9, A1_0 ] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    for (const [key, value] of Object.entries(anomalyData["datos"]["scores"])) {
-      var val = Math.round((value + Number.EPSILON) * 10) / 10;
+    var val = 0
+    for (const [key, value] of Object.entries(
+      anomalyData["datos"]["scores"])) {
+      val = Math.round((value + Number.EPSILON) * 10) / 10;
       
       if (val == -1) {
         A_1_0 += 1;
@@ -141,15 +148,93 @@ function Chart3() {
     }
 
     // #5 Recargar la gráfica con los datos obtenidos
-    setData(
-      {
-        name: '-1',
-        number: A_1_0
+    // setData(
+    //   )
+    setData([{
+         name: '-1',
+         number: A_1_0
+       },
+       {
+         name: '-0.9',
+         number: A_0_9
+       },
+       {
+        name: '-0.8',
+        number: A_0_8
       },
       {
-        name: '-0.9',
-        number: A_0_9
-      })
+        name: '-0.7',
+        number: A_0_7
+      },
+      {
+        name: '-0.6',
+        number: A_0_6
+      },
+      {
+        name: '-0.5',
+        number: A_0_5
+      },
+      {
+        name: '-0.4',
+        number: A_0_4
+      },
+      {
+        name: '-0.3',
+        number: A_0_3
+      },
+      {
+        name: '-0.2',
+        number: A_0_2
+      },
+      {
+        name: '-0.1',
+        number: A_0_1
+      },
+      {
+        name: '0.0',
+        number: A0
+      },
+      {
+        name: '0.1',
+        number: A0_1
+      },
+      {
+        name: '0.2',
+        number: A0_2
+      },
+      {
+        name: '0.3',
+        number: A0_3
+      },
+      {
+        name: '0.4',
+        number: A0_4
+      },
+      {
+        name: '0.5',
+        number: A0_5
+      },
+      {
+        name: '0.6',
+        number: A0_6
+      },
+      {
+        name: '0.7',
+        number: A0_7
+      },
+      {
+        name: '0.8',
+        number: A0_8
+      },
+      {
+        name: '0.9',
+        number: A0_9
+      },
+      {
+        name: '1.0',
+        number: A1_0
+      }
+    ])
   }, [anomalyData]);
 
 	return (
@@ -171,14 +256,7 @@ function Chart3() {
           bottom: 32,
         }}
         >
-<<<<<<< HEAD
           <Bar dataKey="number" fill="#ffba26" />
-=======
-          <XAxis />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="Anomalías" fill="#ffba26" />
->>>>>>> a6827494f5faccd410f6c81a58561cbe9c8377eb
         </BarChart>
         </ResponsiveContainer>
         </div>
